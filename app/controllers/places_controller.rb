9e410883c2f157c75719fb86233f5c9af594ad69
daniel-concha <daniel.concha@kellogg.northwestern.edu>
2022-05-26 20:37:49 +0000

    improving

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"], "user_id" => session["id"] })
+    @posts = Post.where({ "place_id" => @place["id"]})#, "user_id" => session["id"] })
   end
 
   def new
@@ -16,6 +16,7 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["place"]["name"]
+    @place["user_id"] = @current_user["id"]
     @place.save
     redirect_to "/places"
   end
