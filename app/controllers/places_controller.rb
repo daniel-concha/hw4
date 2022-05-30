f0bf0d1e31cadf9c75e982d0845908c0e0a4f4a3
daniel-concha <daniel.concha@kellogg.northwestern.edu>
2022-05-30 16:27:18 +0000

    Fixed sessions and current user issue

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,12 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.where({ "user_id" => @current_user["id"]})
+    @places = Place.all
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "user_id" => session["id"], "place_id" => @place["id"]})#, "user_id" => session["id"] })
+    @posts = Post.where({ "user_id" => @current_user["id"], "place_id" => @place["id"]})#, "user_id" => session["id"] })
   end
 
   def new
@@ -16,7 +16,6 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["place"]["name"]
-    @place["user_id"] = @current_user["id"]
     @place.save
     redirect_to "/places"
   end
